a0e442799d3d38e75bfa271bd7848041381d8ca1
Stcof <Stcof@LAPTOP-G9VQOOCE>
2017-02-17 15:03:47 -0600

    Initialized the Board Panel that will display the game

--- TripleS_Chess/src/triples_chess/Board.java
+++ TripleS_Chess/src/triples_chess/Board.java
@@ -4,11 +4,56 @@
  * and open the template in the editor.
  */
 package triples_chess;
-
+import javax.swing.*;
+import java.awt.*;
 /**
  *
  * @author Stcof
  */
-public class Board {
+public class Board extends JFrame
+{
+    public Board()
+    {
+        int Row, Col;
+        
+        // Implement toolkit and get screen Size for Frame
+        Toolkit aKit = Toolkit.getDefaultToolkit();
+        Dimension XY = aKit.getScreenSize       ();
+        
+        // Set max Fram Measurements to Row and Col
+        Row = XY.width ;
+        Col = XY.height;
+        
+        // Set the Size and Location for the Frame
+        setSize	(25*Row/80	, Col/2		);
+   	setLocation(11*Row/32	, Col/4		);
+        
+        // Set the Title for the Frame
+        setTitle ("Triple S Chess Game!");
+        
+        boardPanel startPane = new boardPanel();
+        Container contentStartPane = getContentPane();
+        contentStartPane.add(startPane);
+        
+    }
+        
+    private class boardPanel extends JPanel
+    {
+        public boardPanel()
+        {
+            
+        }
+        
+            
+    }
+        
+    
+    JPanel gamePanel = new JPanel();
+    
+    JButton peiceButton[][] = new JButton[8][8];
+    
+    String gamePeices = " ";
+    
+    private String canCapture;
     
 }
